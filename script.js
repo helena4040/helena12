cc70a5bc3e5a0a7ea657ea3e7a628c668b692478
helena4040 <135758463+helena4040@users.noreply.github.com>
2024-07-01 15:17:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
@@ -42,4 +42,3 @@ const perguntas = [
         ]
     }
 ]
- 
